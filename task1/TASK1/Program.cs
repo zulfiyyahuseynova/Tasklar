4cfa117a2da9fcef26b40676a9e333c6670462a9
Zulfiyya Huseynova <tu7kswf4d@code.edu.az>
2022-03-04 12:16:33 +0400

    added tasks

---
+++ task1/TASK1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TASK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 1; i < 874; i++)
+            {
+                if (i%7==0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("sayi:" + count);
+        }
+    }
+}
---
+++ task2/TASK2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TASK2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month;
+            Console.WriteLine("Ay daxil edin");
+            month = Console.ReadLine();
+
+            switch (month)
+            {
+                case "yanvar":
+                case "mart":
+                case "may":
+                case " iyul":
+                case "avqust":
+                case "oktyabr":
+                case "dekabr":
+                    Console.WriteLine("bu ayda 31 gun var");
+                    break;
+
+                case "aprel":
+                case "iyun":
+                case "sentyabr":
+                case "noyabr":
+                    Console.WriteLine("bu ayda 30 gun var");
+                    break;
+
+                case "fevral":
+                    Console.WriteLine("bu ayda ya 28 ya da 29 gun ola biler");
+                    break;
+
+                default:
+                    Console.WriteLine("bele bir ay yoxdur");
+                    break;
+
+            }
+        }
+    }
+}
